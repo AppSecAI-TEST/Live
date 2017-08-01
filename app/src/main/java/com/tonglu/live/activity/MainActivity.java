966900e74663bce95ac1e8df3ea7dd11934bbe4d
gaochunchun <jz_gaochun@163.com>
2017-08-01 16:12:35 +0800

    完成直播列表数据展示及播放

--- app/src/main/java/com/tonglu/live/activity/MainActivity.java
+++ app/src/main/java/com/tonglu/live/activity/MainActivity.java
@@ -1,15 +1,19 @@
 package com.tonglu.live.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Base64;
+import android.view.View;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.tonglu.live.R;
+import com.tonglu.live.adapter.LiveAdapter;
 import com.tonglu.live.base.BaseTitleActivity;
 import com.tonglu.live.callback.StringDialogCallback;
 import com.tonglu.live.manager.GenericRequestManager;
 import com.tonglu.live.model.LiveListInfo;
-import com.tonglu.live.utils.Base64Object;
 import com.tonglu.live.utils.Constants;
 import com.tonglu.live.utils.GsonConvertUtil;
 import com.tonglu.live.utils.MD5Utils;
@@ -17,12 +21,15 @@ import com.tonglu.live.utils.ToastUtils;
 import com.tonglu.okhttp.model.Response;
 import com.tonglu.okhttp.utils.OkLogger;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import static com.tonglu.live.utils.MD5Utils.MD5;
-
 public class MainActivity extends BaseTitleActivity {
+
+    private RecyclerView mRecyclerView;
+    private LiveAdapter mLiveAdapter;
+    public List<LiveListInfo.RecordsBean> recordsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +37,33 @@ public class MainActivity extends BaseTitleActivity {
         setContentView(R.layout.activity_main);
         setTitle("直播列表");
 
-       /* final String url = "rtmp://live.otofuturestore.com/malls/dianneizhibo?auth_key=1502265257-0-0-d56929ae83d1de1a1c1683e7cd2bc9cb";
+        mRecyclerView = findView(R.id.rv_list);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        Button mBtn = (Button) findViewById(R.id.btn);
-        mBtn.setOnClickListener(new View.OnClickListener() {
+        mLiveAdapter = new LiveAdapter();
+        mLiveAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                //ToastUtils.showLongToastSafe(position + "");
+
+                //获取直播地址
+                String url = recordsList.get(position).urls.get(0).url;
+                //OkLogger.e(url);
                 Intent intent = new Intent(MainActivity.this, LivePlayerActivity.class);
                 intent.putExtra("url_address", url);
                 startActivity(intent);
             }
-        });*/
+        });
 
-        test();
+        getLiveList();  //请求直播地址
 
+        mRecyclerView.setAdapter(mLiveAdapter);
     }
 
 
-    private void test() {
+    private void getLiveList() {
         String url = "v/VedioAddress";
-
 
         Map<String, String> params = new TreeMap<>();
         params.put("appkey", Constants.APP_KEY);
@@ -67,6 +81,14 @@ public class MainActivity extends BaseTitleActivity {
 
                 if (!TextUtils.isEmpty(response.body())) {
                     LiveListInfo listInfo = GsonConvertUtil.fromJson(response.body(), LiveListInfo.class);
+                    if (listInfo.IsSuccess) {
+                        if (listInfo.records.size() > 0) {
+                            recordsList = listInfo.records;
+                            mLiveAdapter.addData(listInfo.records); //展示数据
+                        }
+                    } else {
+                        ToastUtils.showLongToastSafe("请求失败！");
+                    }
                     //OkLogger.e("------>" + listInfo);
                 }
 
@@ -75,7 +97,7 @@ public class MainActivity extends BaseTitleActivity {
             @Override
             public void onError(Response<String> response) {
                 super.onError(response);
-                ToastUtils.showLongToastSafe("请确认服务器是否开启");
+                ToastUtils.showLongToastSafe("请确认服务器是否开启！");
             }
         });
 
---
+++ app/src/main/java/com/tonglu/live/adapter/LiveAdapter.java
@@ -0,0 +1,46 @@
+package com.tonglu.live.adapter;
+
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.tonglu.live.R;
+import com.tonglu.live.model.LiveListInfo;
+
+public class LiveAdapter extends BaseQuickAdapter<LiveListInfo.RecordsBean, BaseViewHolder> {
+
+    public LiveAdapter() {
+        super(R.layout.layout_livelist);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, LiveListInfo.RecordsBean item) {
+
+        helper.setText(R.id.tv_address, item.vedioName);
+        helper.setText(R.id.tv_vedio_url_type, "直播格式：" + item.urls.get(0).mediaType);
+        helper.setText(R.id.tv_vedio_url, "地址：" + item.urls.get(0).url);
+
+        /*new Resolution { Id = Resolution.IdOriginal, Name = "原画" },
+        new Resolution { Id = "LHD", Name = "高清" },
+        new Resolution { Id = "LSD", Name = "标清" },
+        new Resolution { Id = "LLD", Name = "流畅" },*/
+
+        switch (item.vedioType){
+            case "Original":
+                helper.setText(R.id.tv_vedio_type, "原画");
+                break;
+            case "LHD":
+                helper.setText(R.id.tv_vedio_type,"高清");
+                break;
+            case "LSD":
+                helper.setText(R.id.tv_vedio_type, "标清");
+                break;
+            case "LLD":
+                helper.setText(R.id.tv_vedio_type,"流畅" );
+                break;
+        }
+
+
+    }
+
+
+}
